1a934489b31a62af949a48e4cff1299e30a94209
jodog59 <anderson.jordan59@gmail.com>
2017-02-23 20:48:07 -0700

    Add comments to the box2 design to clarify

--- Pynq-Z1/vivado/Partial_Designs/Source/box2.vhd
+++ Pynq-Z1/vivado/Partial_Designs/Source/box2.vhd
@@ -33,7 +33,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Video_Box is
 port (
-    --reg in
+    -- Registers In
      slv_reg0 : in std_logic_vector(31 downto 0);  
      slv_reg1 : in std_logic_vector(31 downto 0);  
      slv_reg2 : in std_logic_vector(31 downto 0);  
@@ -43,7 +43,7 @@ port (
      slv_reg6 : in std_logic_vector(31 downto 0);  
      slv_reg7 : in std_logic_vector(31 downto 0);    
      
-    --reg out
+    -- Registers Out
     slv_reg0out : out std_logic_vector(31 downto 0);  
     slv_reg1out : out std_logic_vector(31 downto 0);  
     slv_reg2out : out std_logic_vector(31 downto 0);  
@@ -53,9 +53,10 @@ port (
     slv_reg6out : out std_logic_vector(31 downto 0);  
     slv_reg7out : out std_logic_vector(31 downto 0);
     
-    --Bus Clock
+    -- Bus Clock
     CLK : in std_logic;
-    --Video
+	
+    -- Video Input Signals
     RGB_IN_I : in std_logic_vector(23 downto 0); -- Parallel video data (required)
     VDE_IN_I : in std_logic; -- Active video Flag (optional)
     HB_IN_I : in std_logic; -- Horizontal blanking signal (optional)
@@ -63,7 +64,8 @@ port (
     HS_IN_I : in std_logic; -- Horizontal sync signal (optional)
     VS_IN_I : in std_logic; -- Veritcal sync signal (optional)
     ID_IN_I : in std_logic; -- Field ID (optional)
-    --  additional ports here
+    
+	-- Video Output Signals
     RGB_IN_O : out std_logic_vector(23 downto 0); -- Parallel video data (required)
     VDE_IN_O : out std_logic; -- Active video Flag (optional)
     HB_IN_O : out std_logic; -- Horizontal blanking signal (optional)
@@ -71,38 +73,61 @@ port (
     HS_IN_O : out std_logic; -- Horizontal sync signal (optional)
     VS_IN_O : out std_logic; -- Veritcal sync signal (optional)
     ID_IN_O : out std_logic; -- Field ID (optional)
-    
+
+	-- Pixel Clock
     PIXEL_CLK_IN : in std_logic;
-    
+	
+	-- Signals that give the x and y coordinates of the current pixel    
     X_Cord : in std_logic_vector(15 downto 0);
     Y_Cord : in std_logic_vector(15 downto 0)
 
 );
 end Video_Box;
 
+-- Begin Box architecture
 architecture Behavioral of Video_Box is
 
+	-- X location signals
     signal x,x1,x2 : signed(15 downto 0);
+	-- Y location signals
     signal y,y1,y2,diff : signed(15 downto 0);
 
 begin
     
+	-- Get the pixel's x and y coordinates
     x <= signed(x_cord);
     y <= signed(y_cord);
+	
+	-- Get the left and right x coordinates from the registers
 	x1 <= signed(slv_reg0(15 downto 0));
 	x2 <= signed(slv_reg1(15 downto 0));
+	-- Get the top and bottom y coordinates from the registers
 	y1 <= signed(slv_reg2(15 downto 0));
 	y2 <= signed(slv_reg3(15 downto 0));
+	
+	-- Get the size of the lines desired by reading register 4
 	diff <= signed(slv_reg4(15 downto 0));
 
 
+	-- Create a variable width pixel box at the register defined locations
+	-- When the display isn't being written to, write 0's
     RGB_IN_O 	<= (others => '0') when VDE_IN_I = '0' else
+				-- if the y value is greater than y1-diff and less than y1+diff,
+				-- then make change the color to the line's color
                    x"FF0000" when (y > (y1-diff) and y < (y1+diff)) else
+				-- if the y value is greater than y2-diff and less than y2+diff,
+				-- then make change the color to the line's color
                    x"00FF00" when (y > (y2-diff) and y < (y2+diff)) else
+				-- if the x value is greater than x1-diff and less than x1+diff,
+				-- then make change the color to the line's color
                    x"0000FF" when (x > (x1-diff) and x < (x1+diff)) else
+				-- if the x value is greater than x2-diff and less than x2+diff,
+				-- then make change the color to the line's color
                    x"FF00FF" when (x > (x2-diff) and x < (x2+diff)) else
+				-- Otherwise, pass the RGB value straight through
                    RGB_IN_I; 
                    
+	-- Pass the other video signals straight through
     VDE_IN_O	<= VDE_IN_I;
     HB_IN_O		<= HB_IN_I;
     VB_IN_O		<= VB_IN_I;
@@ -110,6 +135,7 @@ begin
     VS_IN_O		<= VS_IN_I;
     ID_IN_O		<= ID_IN_I;
 
+	-- Pass the Register signals straight through
     slv_reg0out <= slv_reg0;
     slv_reg1out <= slv_reg1;
     slv_reg2out <= slv_reg2;
@@ -120,3 +146,4 @@ begin
     slv_reg7out <= slv_reg7;
 
 end Behavioral;
+-- End Box architecture
