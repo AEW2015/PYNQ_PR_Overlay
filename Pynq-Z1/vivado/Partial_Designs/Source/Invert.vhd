73517c587653c99343c7d1fa9b7a177be2a1b8f9
jodog59 <anderson.jordan59@gmail.com>
2017-02-23 17:13:02 -0700

    Added comments to the Invert partial design for clarity

--- Pynq-Z1/vivado/Partial_Designs/Source/Invert.vhd
+++ Pynq-Z1/vivado/Partial_Designs/Source/Invert.vhd
@@ -55,7 +55,8 @@ port (
     
     --Bus Clock
     CLK : in std_logic;
-    --Video
+	
+    --Video input Signals
     RGB_IN_I : in std_logic_vector(23 downto 0); -- Parallel video data (required)
     VDE_IN_I : in std_logic; -- Active video Flag (optional)
     HB_IN_I : in std_logic; -- Horizontal blanking signal (optional)
@@ -63,7 +64,8 @@ port (
     HS_IN_I : in std_logic; -- Horizontal sync signal (optional)
     VS_IN_I : in std_logic; -- Veritcal sync signal (optional)
     ID_IN_I : in std_logic; -- Field ID (optional)
-    --  additional ports here
+	
+    --  Video Output Signals
     RGB_IN_O : out std_logic_vector(23 downto 0); -- Parallel video data (required)
     VDE_IN_O : out std_logic; -- Active video Flag (optional)
     HB_IN_O : out std_logic; -- Horizontal blanking signal (optional)
@@ -72,8 +74,10 @@ port (
     VS_IN_O : out std_logic; -- Veritcal sync signal (optional)
     ID_IN_O : out std_logic; -- Field ID (optional)
     
+	--Pixel Clock 
     PIXEL_CLK_IN : in std_logic;
     
+	--Signals that give the x and y coordinates of the current pixel
     X_Cord : in std_logic_vector(15 downto 0);
     Y_Cord : in std_logic_vector(15 downto 0)
 
@@ -82,18 +86,22 @@ end Video_Box;
 
 architecture Behavioral of Video_Box is
 
-
-
+--Complete RGB value
 signal full_const : unsigned(7 downto 0):= "11111111";
+--Inverted signals for the red, green, and blue values
 signal red_i,green_i,blue_i : unsigned(7 downto 0);
 
 begin
 
+--Take the max value and subtract away the RGB values to invert the image
 red_i <= full_const - unsigned(RGB_IN_I(23 downto 16));
 green_i <= full_const - unsigned(RGB_IN_I(15 downto 8));
 blue_i <= full_const - unsigned(RGB_IN_I(7 downto 0));
 
+--Route the inverted RGB values out
 RGB_IN_O 	<= std_logic_vector(red_i&green_i&blue_i);
+
+--Pass all the other signals through the region
 VDE_IN_O	<= VDE_IN_I;
 HB_IN_O		<= HB_IN_I;
 VB_IN_O		<= VB_IN_I;
@@ -101,6 +109,7 @@ HS_IN_O		<= HS_IN_I;
 VS_IN_O		<= VS_IN_I;
 ID_IN_O		<= ID_IN_I;
 
+--Pass the registers through the region
 slv_reg0out <= slv_reg0;
 slv_reg1out <= slv_reg1;
 slv_reg2out <= slv_reg2;
